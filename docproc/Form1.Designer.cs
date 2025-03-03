e20a6d2d3654fceac62c6225b08732b32d8fa5b6
Rian Hawkins-White <rianandrewwhite@gmail.com>
2025-03-03 09:06:30 -0500

    'init'

---
+++ docproc/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace docproc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.rtxtBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(306, 83);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(75, 23);
+            this.btnTranslate.TabIndex = 0;
+            this.btnTranslate.Text = "translate";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(202, 46);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(201, 20);
+            this.txtFile.TabIndex = 1;
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(409, 43);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFile.TabIndex = 2;
+            this.btnFile.Text = "Add File";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // rtxtBox
+            // 
+            this.rtxtBox.Location = new System.Drawing.Point(12, 142);
+            this.rtxtBox.Name = "rtxtBox";
+            this.rtxtBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.rtxtBox.Size = new System.Drawing.Size(776, 433);
+            this.rtxtBox.TabIndex = 3;
+            this.rtxtBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(844, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Gray";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(942, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "proccess";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::docproc.Properties.Resources._1test;
+            this.pictureBox1.Location = new System.Drawing.Point(844, 142);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(570, 404);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove_1);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1502, 596);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rtxtBox);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnTranslate);
+            this.Name = "Form1";
+            this.Text = "ORC Translate";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.Form1_DragOver);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.RichTextBox rtxtBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ docproc/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+using System.Diagnostics;
+using IronOcr;
+using System.IO;
+
+namespace docproc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string replaceNewLine(String str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                int x = str.IndexOf('\n', i);
+                if (x != -1)
+                {
+                    str = str.Substring(0, x) + "\r\n" + str.Substring(x + 1);
+                    i = x + 2;
+                }
+                else
+                {
+                    return str;
+                }
+            }
+            return str;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //String str = "this\nis\ndumb";
+
+            //txtBox.Text = replaceNewLine(str);
+        }
+
+        private static byte[] ImageToByte2(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+
+        private void proccess()
+        {
+            var testImagePath = txtFile.Text;
+
+            String strSimple = "";
+            try
+            {
+                var logger = new FormattedConsoleLogger();
+                var resultPrinter = new ResultPrinter(logger);
+                using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(testImagePath))
+                    {
+                        using (logger.Begin("Process image"))
+                        {
+                            var i = 1;
+                            using (var page = engine.Process(img))
+                            {
+                                var text = page.GetText();
+                                strSimple += text;
+                                logger.Log("Text: {0}", text);
+                                logger.Log("Mean confidence: {0}", page.GetMeanConfidence());
+
+                                using (var iter = page.GetIterator())
+                                {
+                                    iter.Begin();
+                                    do
+                                    {
+                                        if (i % 2 == 0)
+                                        {
+                                            using (logger.Begin("Line {0}", i))
+                                            {
+                                                do
+                                                {
+                                                    using (logger.Begin("Word Iteration"))
+                                                    {
+                                                        if (iter.IsAtBeginningOf(PageIteratorLevel.Block))
+                                                        {
+                                                            logger.Log("New block");
+                                                        }
+                                                        if (iter.IsAtBeginningOf(PageIteratorLevel.Para))
+                                                        {
+                                                            logger.Log("New paragraph");
+                                                        }
+                                                        if (iter.IsAtBeginningOf(PageIteratorLevel.TextLine))
+                                                        {
+                                                            logger.Log("New line");
+                                                        }
+                                                        logger.Log("word: " + iter.GetText(PageIteratorLevel.Word));
+                                                    }
+                                                } while (iter.Next(PageIteratorLevel.TextLine, PageIteratorLevel.Word));
+                                            }
+                                        }
+                                        i++;
+                                    } while (iter.Next(PageIteratorLevel.Para, PageIteratorLevel.TextLine));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError(e.ToString());
+                Console.WriteLine("Unexpected Error: " + e.Message);
+                Console.WriteLine("Details: ");
+                Console.WriteLine(e.ToString());
+                MessageBox.Show("error");
+            }
+            rtxtBox.Text = strSimple;
+            //Console.Write("Press any key to continue . . . ");
+            //Console.ReadKey(true);
+        }
+
+        private void proccessIOCR()
+        {
+            var ocr = new IronTesseract();
+            using (var ocrInput = new OcrInput())
+            {
+                ocrInput.LoadImage(txtFile.Text);
+                //ocrInput.LoadPdf("document.pdf");
+
+                // Optionally Apply Filters if needed:
+                // ocrInput.Deskew();  // use only if image not straight
+                // ocrInput.DeNoise(); // use only if image contains digital noise
+
+                var ocrResult = ocr.Read(ocrInput);
+                rtxtBox.Text = ocrResult.Text;
+                // Console.WriteLine(ocrResult.Text);
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            txtFile.Text = openFileDialog1.FileName;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                txtFile.Text = files[0];
+                var testImagePath = files[0];
+                pictureBox1.Image = Image.FromFile(testImagePath);               
+            }
+        }
+
+
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void Form1_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            //proccessIOCR();
+            proccess();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics myCanvas = pictureBox1.CreateGraphics();
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Graphics myCanvas = pictureBox1.CreateGraphics();
+            Image img = pictureBox1.Image;
+            Bitmap bimg = new Bitmap(img);
+
+            pictureBox1.Image = bimg;
+            Color c = bimg.GetPixel(e.X, e.Y);
+            Console.WriteLine("color: " + c.A + " r: " + c.R + " g: " + c.G + " b: " + c.B);
+
+        }
+
+        public Bitmap GrayScaleFilter(Bitmap image)
+        {
+            Bitmap grayScale = new Bitmap(image.Width, image.Height);
+
+            for (Int32 y = 0; y < grayScale.Height; y++)
+                for (Int32 x = 0; x < grayScale.Width; x++)
+                {
+                    Color c = image.GetPixel(x, y);
+
+                    Int32 gs = (Int32)(c.R * 0.3 + c.G * 0.59 + c.B * 0.11);
+
+                    grayScale.SetPixel(x, y, Color.FromArgb(gs, gs, gs));
+                }
+            return grayScale;
+        }
+
+        private int sSize = 2;
+        private void pictureBox1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            //Graphics myCanvas = pictureBox1.CreateGraphics();
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Image img = pictureBox1.Image;
+                Bitmap bimg = new Bitmap(img);
+
+                int change = sSize * -1;
+                while (change < sSize)
+                {
+                    int y = e.Y + change;
+                    for (int x = e.X - sSize; x <= (e.X + sSize); x++)
+                    {
+                        if (x >= 0 && x < bimg.Width)
+                        {
+                            if (y >= 0 && y < bimg.Width)
+                            {
+                                bimg.SetPixel(x, y, Color.Black);
+                            }
+                        }
+
+                    }
+                    change++;
+                }
+
+
+                pictureBox1.Image = bimg;
+                Console.WriteLine("mouse2: " + e.Location);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image img = pictureBox1.Image;
+            Bitmap bimg = new Bitmap(img);
+            bimg = GrayScaleFilter(bimg);
+            pictureBox1.Image = bimg;
+        }
+
+        int limit = 145;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Image img = pictureBox1.Image;
+            Bitmap bimg = new Bitmap(img);
+
+            
+
+            for (Int32 y = 0; y < bimg.Height; y++)
+            {
+                for (Int32 x = 0; x < bimg.Width; x++)
+                {
+                    Color c = bimg.GetPixel(x, y);
+                    int change = sSize * -1;
+                    if (c.R < limit || c.G < limit || c.B < limit)
+                    {
+                        bimg.SetPixel(x, y, Color.Black);
+
+                        //while (change < sSize)
+                        //{
+                        //    int iy = y + change;
+                        //    for (int ix = x - sSize; ix <= (x + sSize); ix++)
+                        //    {
+                        //        if (ix >= 0 && ix < bimg.Width)
+                        //        {
+                        //            if (iy >= 0 && iy < bimg.Height)
+                        //            {
+                        //                bimg.SetPixel(ix, iy, Color.Black);
+                        //            }
+                        //        }
+                        //    }
+                        //    change++;
+                        //}
+                    }
+                }
+            }
+            pictureBox1.Image = bimg;
+        }
+    }
+
+    public class ResultPrinter
+    {
+        readonly FormattedConsoleLogger logger;
+
+        public ResultPrinter(FormattedConsoleLogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Print(ResultIterator iter)
+        {
+            logger.Log("Is beginning of block: {0}", iter.IsAtBeginningOf(PageIteratorLevel.Block));
+            logger.Log("Is beginning of para: {0}", iter.IsAtBeginningOf(PageIteratorLevel.Para));
+            logger.Log("Is beginning of text line: {0}", iter.IsAtBeginningOf(PageIteratorLevel.TextLine));
+            logger.Log("Is beginning of word: {0}", iter.IsAtBeginningOf(PageIteratorLevel.Word));
+            logger.Log("Is beginning of symbol: {0}", iter.IsAtBeginningOf(PageIteratorLevel.Symbol));
+
+            logger.Log("Block text: \"{0}\"", iter.GetText(PageIteratorLevel.Block));
+            logger.Log("Para text: \"{0}\"", iter.GetText(PageIteratorLevel.Para));
+            logger.Log("TextLine text: \"{0}\"", iter.GetText(PageIteratorLevel.TextLine));
+            logger.Log("Word text: \"{0}\"", iter.GetText(PageIteratorLevel.Word));
+            logger.Log("Symbol text: \"{0}\"", iter.GetText(PageIteratorLevel.Symbol));
+        }
+    }
+
+}
